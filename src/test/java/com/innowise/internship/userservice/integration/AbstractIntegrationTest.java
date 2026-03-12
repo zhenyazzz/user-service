04c715a5f4ba11566821b71d880c86ca215fe5c1
zhenya <zenamatusev68@gmail.com>
2026-03-12 21:56:25 +0300

    refactor: remove Testcontainers dependency and update datasource configuration for integration tests

--- src/test/java/com/innowise/internship/userservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/innowise/internship/userservice/integration/AbstractIntegrationTest.java
@@ -6,26 +6,13 @@ import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles("integrationtest")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 public abstract class AbstractIntegrationTest {
-
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
-            .withDatabaseName("user_service_test")
-            .withUsername("test")
-            .withPassword("test")
-            .withReuse(true);
 
     @LocalServerPort
     private int port;
