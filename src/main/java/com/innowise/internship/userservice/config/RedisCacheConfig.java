818008fd3293ed1ccc172bff1b87fdd5d746d8ec
zhenya <zenamatusev68@gmail.com>
2026-03-13 17:55:53 +0300

    feat: add caching configuration and properties for application performance optimization

---
+++ src/main/java/com/innowise/internship/userservice/config/RedisCacheConfig.java
@@ -0,0 +1,51 @@
+package com.innowise.internship.userservice.config;
+
+import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import org.springframework.cache.annotation.EnableCaching;
+
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheConfiguration defaultCacheConfiguration(CacheProperties cacheProperties) {
+        GenericJacksonJsonRedisSerializer serializer = GenericJacksonJsonRedisSerializer.builder()
+                .enableDefaultTyping(
+                        BasicPolymorphicTypeValidator.builder()
+                                .allowIfBaseType("com.innowise.internship")
+                                .build())
+                .build();
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(cacheProperties.getDefaultTtl())
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer(
+            RedisCacheConfiguration defaultCacheConfig,
+            CacheProperties cacheProperties) {
+        return builder -> builder
+                .withCacheConfiguration("users",
+                        defaultCacheConfig.entryTtl(cacheProperties.getTtlForCache("users")))
+                .withCacheConfiguration("users_pages",
+                        defaultCacheConfig.entryTtl(cacheProperties.getTtlForCache("users_pages")))
+                .withCacheConfiguration("cards",
+                        defaultCacheConfig.entryTtl(cacheProperties.getTtlForCache("cards")))
+                .withCacheConfiguration("cards_pages",
+                        defaultCacheConfig.entryTtl(cacheProperties.getTtlForCache("cards_pages")))
+                .withCacheConfiguration("cards_user",
+                        defaultCacheConfig.entryTtl(cacheProperties.getTtlForCache("cards_user")));
+    }
+}
